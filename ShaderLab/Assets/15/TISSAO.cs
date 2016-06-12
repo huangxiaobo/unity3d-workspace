926c9112b6300fc2ffbffdb22e96cf443faa6a1a
huangxiaobo <huangxb7818@gmail.com>
2016-06-12 20:12:41 +0800

    Add example of TISSAO
    Add example of TISSAO

---
+++ ShaderLab/Assets/15/TISSAO.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class TISSAO : MonoBehaviour 
+{
+    public Shader TISSAOShader;
+    private Material SSAOMaterial;
+ 
+    void OnEnable()
+    {
+        GetComponent<Camera>().depthTextureMode |= DepthTextureMode.Depth;
+    }
+ 
+    private void CreateMaterial()
+    {
+        if (!TISSAOShader || SSAOMaterial)
+            return;
+        SSAOMaterial = new Material(TISSAOShader) {hideFlags = HideFlags.HideAndDontSave};
+    }
+ 
+    [ImageEffectOpaque]
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        CreateMaterial();
+        Graphics.Blit(source, destination, SSAOMaterial);
+    }
+}

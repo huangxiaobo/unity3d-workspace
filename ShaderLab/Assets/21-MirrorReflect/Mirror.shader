f570773289f930ef2c7b6446277a62743d2f620f
huangxiaobo <huangxb7818@gmail.com>
2016-07-23 17:21:19 +0800

    镜面反射例子增加注释
    镜面反射例子增加注释

--- ShaderLab/Assets/21-MirrorReflect/Mirror.shader
+++ ShaderLab/Assets/21-MirrorReflect/Mirror.shader
@@ -38,8 +38,13 @@
 			v2f vert (appdata v)
 			{
 				v2f o;
+				// 把顶点位置从模型空间转换到投影空间
 				o.vertex = mul(UNITY_MATRIX_MVP, v.vertex);
+				// 计算uv坐标，即根据mesh上的uv坐标来计算真正的纹理上对应的坐标
+				// 宏TRANSFORM_TEX的定义：#define TRANSFORM_TEX(tex,name) (tex.xy*name##_ST.xy+name##_ST.zw)
+				// 故需要在shader中定义一个名为_YourTextureName_ST(也就是纹理名字加一个_ST后缀的变量)
 				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				// 获得一个投影点对应的屏幕坐标点
 				o.refl = ComputeScreenPos(o.vertex);
 				return o;
 			}
@@ -48,8 +53,9 @@
 			{
 				// sample the texture
 				fixed4 col = tex2D(_MainTex, i.uv);
+				// 二维投影纹理查询
 				fixed4 refl = tex2Dproj(_ReflectionTex, UNITY_PROJ_COORD(i.refl));
-				return col * refl;
+				return col * (1 - refl.a) + refl * refl.a;
 			}
 			ENDCG
 		}
--- ShaderLab/Assets/21-MirrorReflect/MirrorReflection.cs
+++ ShaderLab/Assets/21-MirrorReflect/MirrorReflection.cs
@@ -8,7 +8,7 @@ using System.Collections;
 public class MirrorReflection : MonoBehaviour
 {
 	public bool m_DisablePixelLights = true;
-	public int m_TextureSize = 256;
+	public int m_TextureSize = 1024;
 	public float m_ClipPlaneOffset = 0.07f;
  
 	public LayerMask m_ReflectLayers = -1;
@@ -34,15 +34,17 @@ public class MirrorReflection : MonoBehaviour
 		if( !cam )
 			return;
  
-		// Safeguard from recursive reflections.        
+		// Safeguard from recursive reflections.
 		if( s_InsideRendering )
 			return;
 		s_InsideRendering = true;
  
+		// 创建反射绘制纹理和反射摄像机
 		Camera reflectionCamera;
 		CreateMirrorObjects( cam, out reflectionCamera );
  
 		// find out the reflection plane: position and normal in world space
+		// 反射平面在世界坐标中的位置和法线方向
 		Vector3 pos = transform.position;
 		Vector3 normal = transform.up;
  
@@ -51,13 +53,16 @@ public class MirrorReflection : MonoBehaviour
 		if( m_DisablePixelLights )
 			QualitySettings.pixelLightCount = 0;
  
+		// 根据主摄像机更新反射摄像机
 		UpdateCameraModes( cam, reflectionCamera );
  
 		// Render reflection
 		// Reflect camera around reflection plane
+		// 创建反射平面，反射平面与显示投影的平面偏移一点位置
 		float d = -Vector3.Dot (normal, pos) - m_ClipPlaneOffset;
 		Vector4 reflectionPlane = new Vector4 (normal.x, normal.y, normal.z, d);
  
+		// 计算镜像反射矩阵，并计算主摄像机在反射平面的镜像位置，此位置就是反射摄像机的位置
 		Matrix4x4 reflection = Matrix4x4.zero;
 		CalculateReflectionMatrix (ref reflection, reflectionPlane);
 		Vector3 oldpos = cam.transform.position;
@@ -66,20 +71,25 @@ public class MirrorReflection : MonoBehaviour
  
 		// Setup oblique projection matrix so that near plane is our reflection
 		// plane. This way we clip everything below/above it for free.
+		// 计算反射平面在反射摄像机中的位置，然后将这个平面设置成反射摄像机的近裁剪面
 		Vector4 clipPlane = CameraSpacePlane( reflectionCamera, pos, normal, 1.0f );
 		//Matrix4x4 projection = cam.projectionMatrix;
 		Matrix4x4 projection = cam.CalculateObliqueMatrix(clipPlane);
 		reflectionCamera.projectionMatrix = projection;
  
+		// 设置反射摄像机的剔除码，渲染纹理对象，摄像机新位置
+		// 渲染对象
+		// 恢复设置，将渲染纹理设置到材质上
 		reflectionCamera.cullingMask = ~(1<<4) & m_ReflectLayers.value; // never render water layer
 		reflectionCamera.targetTexture = m_ReflectionTexture;
-		GL.SetRevertBackfacing (true);
+		GL.invertCulling = true;
 		reflectionCamera.transform.position = newpos;
 		Vector3 euler = cam.transform.eulerAngles;
+		// !!!这个地方是不是有错误，为什么去掉了x轴旋转分量
 		reflectionCamera.transform.eulerAngles = new Vector3(0, euler.y, euler.z);
 		reflectionCamera.Render();
 		reflectionCamera.transform.position = oldpos;
-		GL.SetRevertBackfacing (false);
+		GL.invertCulling = false;
 		Material[] materials = rend.sharedMaterials;
 		foreach( Material mat in materials ) {
 			if( mat.HasProperty("_ReflectionTex") )
@@ -87,6 +97,7 @@ public class MirrorReflection : MonoBehaviour
 		}
  
 		// Restore pixel light count
+		// 恢复逐像素灯光数量
 		if( m_DisablePixelLights )
 			QualitySettings.pixelLightCount = oldPixelLightCount;
  
@@ -106,14 +117,15 @@ public class MirrorReflection : MonoBehaviour
 		m_ReflectionCameras.Clear();
 	}
  
- 
+	// 更新反射摄像机
+	// 将当前的主摄像机的参数同步到反射摄像机
 	private void UpdateCameraModes( Camera src, Camera dest )
 	{
 		if( dest == null )
 			return;
 		// set camera to clear the same way as current camera
 		dest.clearFlags = src.clearFlags;
-		dest.backgroundColor = src.backgroundColor;        
+		dest.backgroundColor = src.backgroundColor;
 		if( src.clearFlags == CameraClearFlags.Skybox )
 		{
 			Skybox sky = src.GetComponent(typeof(Skybox)) as Skybox;
@@ -140,11 +152,14 @@ public class MirrorReflection : MonoBehaviour
 	}
  
 	// On-demand create any objects we need
+	// 创建一个绘制镜像的摄像机
 	private void CreateMirrorObjects( Camera currentCamera, out Camera reflectionCamera )
 	{
 		reflectionCamera = null;
  
 		// Reflection render texture
+		// 如果当前纹理为空，或者旧纹理大小不等于当前纹理大小
+		// 清除并重新创建反射纹理
 		if( !m_ReflectionTexture || m_OldReflectionTextureSize != m_TextureSize )
 		{
 			if( m_ReflectionTexture )
@@ -157,6 +172,8 @@ public class MirrorReflection : MonoBehaviour
 		}
  
 		// Camera for reflection
+		// 创建反射摄像机
+		// 反射摄像机的位置在投影平面的位置
 		reflectionCamera = m_ReflectionCameras[currentCamera] as Camera;
 		if( !reflectionCamera ) // catch both not-in-dictionary and in-dictionary-but-deleted-GO
 		{
@@ -168,7 +185,7 @@ public class MirrorReflection : MonoBehaviour
 			reflectionCamera.gameObject.AddComponent<FlareLayer>();
 			go.hideFlags = HideFlags.HideAndDontSave;
 			m_ReflectionCameras[currentCamera] = reflectionCamera;
-		}        
+		}
 	}
  
 	// Extended sign: returns -1, 0 or 1 based on sign of a
@@ -180,6 +197,7 @@ public class MirrorReflection : MonoBehaviour
 	}
  
 	// Given position/normal of the plane, calculates plane in camera space.
+	// 给定一个平面的位置和法向量，计算这个平面在摄像机空间的位置
 	private Vector4 CameraSpacePlane (Camera cam, Vector3 pos, Vector3 normal, float sideSign)
 	{
 		Vector3 offsetPos = pos + normal * m_ClipPlaneOffset;
@@ -190,6 +208,8 @@ public class MirrorReflection : MonoBehaviour
 	}
  
 	// Calculates reflection matrix around the given plane
+	// 计算一个平面的反射矩阵
+	// 一个点可以通过这个矩阵变换到此平面的镜像位置
 	private static void CalculateReflectionMatrix (ref Matrix4x4 reflectionMat, Vector4 plane)
 	{
 		reflectionMat.m00 = (1F - 2F*plane[0]*plane[0]);

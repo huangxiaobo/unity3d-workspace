75710967820fdc47d525dbe2c6b92d8c78270e33
huangxiaobo <huangxb7818@gmail.com>
2016-08-03 01:53:39 +0800

    角色遮挡后半透明的例子
    角色遮挡后半透明的例子

---
+++ ShaderLab/Assets/22-Occlusion/Occlusion.shader
@@ -0,0 +1,90 @@
+﻿Shader "ShaderLib/22/Occlusion"
+{
+	Properties
+	{
+		_MainTex("Base (RGB)", 2D) = "white" {}
+		_DepthMap("DepthMap", 2D) = "white" {}
+		_OcclusionMap("OcclusionMap (RGB)", 2D) = "white" {}
+		_Intensity("Intensity", Float) = 0.0
+		_Tiling("Tiling", Vector) = (1.0, 1.0, 0.0, 0.0)
+	}
+
+	SubShader
+	{
+		Pass
+		{
+			ZTest Always
+			ZWrite Off
+			Cull Off
+			Fog{ Mode Off }
+
+			CGPROGRAM
+			#include "UnityCG.cginc"
+			#pragma vertex vert
+			#pragma fragment frag
+			#pragma fragmentoption ARB_precision_hint_fastest
+
+			uniform sampler2D _MainTex;
+			uniform sampler2D _DepthMap;
+			uniform sampler2D _OcclusionMap;
+			uniform sampler2D _CameraDepthNormalsTexture;
+			fixed4 _MainTex_TexelSize;
+			fixed _Intensity;
+			fixed _Power;
+			fixed4 _Tiling;
+
+			struct a2v
+			{
+				fixed4 vertex : POSITION;
+				fixed2 texcoord : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				fixed4 vertex : SV_POSITION;
+				fixed2 uv : TEXCOORD0;
+			};
+
+			v2f vert(a2v v)
+			{
+				v2f o;
+				o.vertex = mul(UNITY_MATRIX_MVP, v.vertex);
+				o.uv = v.texcoord.xy;
+
+				return o;
+			}
+
+			fixed4 frag(v2f i) : SV_Target
+			{
+				fixed4 c = tex2D(_MainTex, i.uv);
+				float2 uv = float2(i.uv.x, 1 - i.uv.y);
+
+				float depth;
+				float3 normal;
+				// DepthMap为单独绘制角色而来的深度法线图
+				DecodeDepthNormal(tex2D(_DepthMap, uv), depth, normal);
+
+				// CameraDepthNormalsTexture 是摄像机的深度纹理图
+				fixed4 cameraDepthMap = tex2D(_CameraDepthNormalsTexture, uv);
+				fixed cameraDepth = DecodeFloatRG(cameraDepthMap.zw);
+
+				fixed4 o = c;
+				// 将单独绘制的深度和摄像机的深度相比较，就可以知道角色哪一部分被遮挡了
+				if (depth > 0 && cameraDepth < depth)
+				{
+					fixed2 uv = i.uv * _Tiling.xy + _Tiling.zw;
+					fixed3 color = tex2D(_OcclusionMap, uv);
+					fixed nf = saturate(dot(normal, fixed3(0, 0, 1)));
+					nf = pow(nf, _Intensity);
+					o.rgb = lerp(color, c.rgb, nf);
+				}
+
+				return o;
+			}
+
+			ENDCG
+		}
+	}
+
+	Fallback off
+}
---
+++ ShaderLab/Assets/22-Occlusion/OcclusionEffect.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+[ExecuteInEditMode]
+public class OcclusionEffect : MonoBehaviour
+{
+	private const string NODE = "Occlusion Camera";
+
+	[Range(0.0f, 10.0f)]
+	public float intensity = 1.0f;
+	public Vector4 tiling = new Vector4(1, 1, 0, 0);
+	public Texture2D occlusionMap;
+	public LayerMask cullingMask;
+
+	private Camera occlusionCamera
+	{
+		get
+		{
+			if (null == m_OcclusionCamera)
+			{
+				Transform node = transform.FindChild(NODE);
+				if (null == node)
+				{
+					node = new GameObject(NODE).transform;
+					node.parent = transform;
+					node.localPosition = Vector3.zero;
+					node.localRotation = Quaternion.identity;
+					node.localScale = Vector3.one;
+				}
+
+				m_OcclusionCamera = node.GetComponent<Camera>();
+				if (null == m_OcclusionCamera)
+				{
+					m_OcclusionCamera = node.gameObject.AddComponent<Camera>();
+				}
+
+				m_OcclusionCamera.enabled = false;
+				m_OcclusionCamera.clearFlags = CameraClearFlags.SolidColor;
+				m_OcclusionCamera.backgroundColor = new Color(0, 0, 0, 0);
+				m_OcclusionCamera.renderingPath = RenderingPath.VertexLit;
+				m_OcclusionCamera.hdr = false;
+				m_OcclusionCamera.useOcclusionCulling = false;
+				m_OcclusionCamera.gameObject.hideFlags = HideFlags.HideAndDontSave;
+			}
+
+			return m_OcclusionCamera;
+		}
+	}
+	private Camera m_OcclusionCamera;
+
+	private Material occlusionMaterial
+	{
+		get
+		{
+			if (m_OcclusionMaterial == null)
+			{
+				m_OcclusionMaterial = new Material(Shader.Find("ShaderLib/22/Occlusion"));
+				m_OcclusionMaterial.hideFlags = HideFlags.HideAndDontSave;
+			}
+
+			return m_OcclusionMaterial;
+		}
+	}
+	private Material m_OcclusionMaterial = null;
+
+	private RenderTexture depthMap;
+
+	void Start()
+	{
+	}
+
+	private void OnPreRender()
+	{
+		depthMap = RenderTexture.GetTemporary(Screen.width, Screen.height, 16, RenderTextureFormat.ARGB32);
+
+		GetComponent<Camera>().depthTextureMode = DepthTextureMode.DepthNormals;
+
+		occlusionCamera.depthTextureMode |= DepthTextureMode.DepthNormals;
+		occlusionCamera.fieldOfView = GetComponent<Camera>().fieldOfView;
+		occlusionCamera.orthographic = GetComponent<Camera>().orthographic;
+		occlusionCamera.nearClipPlane = GetComponent<Camera>().nearClipPlane;
+		occlusionCamera.farClipPlane = GetComponent<Camera>().farClipPlane;
+		occlusionCamera.cullingMask = cullingMask; // 这个地方要只留下角色相关的图层，其余图层要剔除掉
+		occlusionCamera.targetTexture = depthMap;
+		occlusionCamera.RenderWithShader(Shader.Find("Hidden/Camera-DepthNormalTexture"), string.Empty);
+	}
+
+	private void OnPostRender()
+	{
+		RenderTexture.ReleaseTemporary(depthMap);
+	}
+
+	[ImageEffectOpaque]
+	private void OnRenderImage(RenderTexture sourceTexture, RenderTexture destTexture)
+	{
+		occlusionMaterial.SetTexture("_DepthMap", depthMap);
+		occlusionMaterial.SetTexture("_OcclusionMap", occlusionMap);
+		occlusionMaterial.SetFloat("_Intensity", intensity);
+		occlusionMaterial.SetVector("_Tiling", tiling);
+		Graphics.Blit(sourceTexture, destTexture, occlusionMaterial);
+	}
+
+	private void OnDisable()
+	{
+		OnDestroy();
+	}
+
+	private void OnDestroy()
+	{
+		if (null != m_OcclusionCamera)
+		{
+			if (Application.isPlaying)
+			{
+				Destroy(m_OcclusionCamera.gameObject);
+			}
+			else
+			{
+				DestroyImmediate(m_OcclusionCamera.gameObject);
+			}
+		}
+	}
+}
